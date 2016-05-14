5230a489e18b69fad19955fb2b0cedbdaf4de61b
hika, maeng <hika00@gmail.com>
2016-05-15 01:35:51 +0900

    Update 09_WeakSet.js

--- es6for3/09_WeakSet.js
+++ es6for3/09_WeakSet.js
@@ -1,9 +1,13 @@
 if(!window.WeakSet) (function(){
 	var KEY = Symbol(), SET = Symbol(), WSet = function(a){
-		var wset = this[SET] = [], self = this;
-		if(a instanceof Array) a.forEach(function(v){
-			if(v instanceof Object) self.add(v);
-		});
+		var wset = this[SET] = [], self = this, v, cnt = 10000;
+		if(typeof a[Symbol.iterator] == 'function') a = a[Symbol.iterator]();
+		else if(typeof a.next != 'function') return;
+		do{
+			v = a.next();
+			if(v.done) break;
+			wset[wset.length] = v.value;
+		}while(cnt--)
 		Object.freeze(this);
 	}, fn = WSet.prototype;
 	fn.has = function(v){return this[SET].indexOf(v) !== -1;};
