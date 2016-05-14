b9cc25e2ac124d95ef8d7c6e23c7d30f13a0c7df
hika, maeng <hika00@gmail.com>
2016-05-15 00:44:30 +0900

    Create 00_Symbol.js

---
+++ es6for3/00_Symbol.js
@@ -0,0 +1,20 @@
+"use strict"
+if(typeof Object.freeze != 'function') Object.freeze = function(v){return v;};
+if(!window.Symbol)(function(){
+	var uuid = 0,
+		Sym = function(id){this[ID] = id;},
+		Symbol = function(i, v){
+			return Object.freeze(new Sym('@@Symbol@@::' + (uuid++));
+		},
+		ID = new Sym(),
+		keys = {},
+		syms = {};
+	Sym.prototype.toString = function(){return this[ID];},
+	Symbol.for = function(k){
+		if(!keys[k]) syms[keys[k] = Symbol()] = k;
+		return keys[k];
+	};
+	Symbol.keyFor = function(s){return syms[s];};
+	Symbol.iterator = Object.freeze(new Sym('@@iterator'));
+	window.Symbol = Object.freeze(Symbol);
+})();
