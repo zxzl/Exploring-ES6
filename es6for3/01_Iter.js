93761c1083ed6d42c6f878724e0fe24ddb0efb02
hika, maeng <hika00@gmail.com>
2016-05-16 16:34:43 +0900

    Update 01_Iter.js

--- es6for3/01_Iter.js
+++ es6for3/01_Iter.js
@@ -13,37 +13,30 @@ if(!window.Iter)(function(){
 	};
 	window.Iter = Iter;
 })();
-if(!Array.prototype.forEach)(function(){
-	var fn = Array.prototype, Iter;
-	fn.forEach = function(f){
-		var i, j;
-		for(i = 0, j = this.length; i < j; i++) f(this[i], i, this);
+(function(){
+	var fn = Array.prototype;
+	if(!fn.forEach) fn.forEach = function(f){
+		for(var i = 0, j = this.length; i < j; i++) f(this[i], i, this);
 	};
-	fn.map = function(f){
-		var r = [], i, j;
-		for(i = 0, j = this.length; i < j; i++) r[i] = f(this[i], i, this);
+	if(!fn.map)  = function(f){
+		for(var r = [], i = 0, j = this.length; i < j; i++) r[i] = f(this[i], i, this);
 		return r;
 	};
-	fn.filter = function(f){
-		var r = [], i, j;
-		for(i = 0, j = this.length; i < j; i++) if(f(this[i], i, this)) r[r.length] = this[i];
+	if(!fn.filter) fn.filter = function(f){
+		for(var r = [], i = 0, j = this.length; i < j; i++) if(f(this[i], i, this)) r[r.length] = this[i];
 		return r;
 	};
-	fn.reduce = function(){
-		var f = arguments[0], r, i = 0, j = this.length;
-		if(arguments.length == 2){
-			r = arguments[1];
-		}else{
-			r = this[i++];
-		}
-		for(; i < j; i++) r = f(r, this[i], i, this);
+	if(!fn.reduce) fn.reduce = function(){
+		for(var f = arguments[0], i = 0, j = this.length, r = arguments.length == 2 ? arguments[1] : this[i++]; i < j; i++) r = f(r, this[i], i, this);
 		return r;
 	};
-	fn['@@iterator'] = function(){return new Iter(this);};
+	if(!fn[Symbol.iterator]) fn[Symbol.iterator] = function(){return new Iter(this);};
 	if(typeof Array.from != 'function') Array.from = function(v){
 		var r = [], i;
-		if('@@iterator' in v){
-			v = v['@@iterator']();
+		if(Symbol.iterator in v){
+			v = v[Symbol.iterator]();
+		}
+		if('next' in v){
 			do{
 				i = v.next();
 				if(i.done) break;
