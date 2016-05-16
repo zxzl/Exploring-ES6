759f4e6589269bb6c382098264f840e261129dff
hika, maeng <hika00@gmail.com>
2016-05-17 00:24:55 +0900

    Update 06_Map.js

--- es6for3/06_Map.js
+++ es6for3/06_Map.js
@@ -1,11 +1,11 @@
-if(!window.Map) (function(){
+if(!window.Map || (new Map([['a', 3]])).get('a') != 3)(function(){
 	var MAP = Symbol(), Map = function(a){
 		var map = this[MAP] = {}, self = this;
-		if(a instanceof Array) a.forEach(function(v){self.set(v[0], v[1]);});
+		if(a instanceof Array) a.forEach(function(v){map[v[0]] = v[1];});
 		Object.freeze(this);
 	}, fn = Map.prototype;
 	fn.set = function(k, v){this[MAP][k] = v;};
-	fn.get = function(k){return this[MAP][k];};
+	fn.get = function(k){return this.has(k) ? this[MAP][k] : undefined;};
 	fn.has = function(k){return this[MAP].hasOwnProperty(k);};
 	fn.del = function(k){delete this[MAP][k];};
 	fn.entries = function(){
