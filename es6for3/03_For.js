53081e4d61ce94bca39a6b60305f3a3bbeb70afd
hika, maeng <hika00@gmail.com>
2016-05-16 21:58:15 +0900

    Update 03_For.js

--- es6for3/03_For.js
+++ es6for3/03_For.js
@@ -1,14 +1,14 @@
-window.For = (function(){
+(function(){
 	var DEST = Symbol(), Of;
 	Of = function(d){this[DEST] = d;};
-	Of.prototype.of = function(iter, f){
+	Of.prototype.Of = function(iter, f){
 		var cnt = 100000, val, dest = this[DEST];
-		iter = iter['@@iterator']();
+		if(typeof iter.next != 'function') iter = iter['@@iterator']();
 		while(cnt--){
 			val = iter.next();
 			if(val.done) break;
-			else f(null, Dest(val.value));
+			else f(Dest(dest, val.value));
 		}
 	};
-	return function(dest){return new Of(dest);};
+	window.For = function(dest){return new Of(dest);};
 })();
