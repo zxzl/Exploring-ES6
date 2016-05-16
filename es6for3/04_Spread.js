0d44c263bd234365dc1c037685afbbe34d870aed
hika, maeng <hika00@gmail.com>
2016-05-17 00:24:39 +0900

    Update 04_Spread.js

--- es6for3/04_Spread.js
+++ es6for3/04_Spread.js
@@ -1,16 +1,11 @@
-window.Spread = (function(){
-	var sp;
-	sp = function(r, a){
-		var i, j, k, l, v;
-		for(i = 0, j = a.length; i < j; i++){
-			if(a[i] == '...'){
-				sp(r, a[++i]);
-			}else{
-				r[r.length] = a[i];
+if(!window.Spread)(function(){
+	var sp = function(r, a){
+			for(var i = 0, j = a.length; i < j; i++){
+				if(a[i] == '...') sp(r, a[++i]);
+				else r[r.length] = a[i];
 			}
-		}
-	};
-	return function(a){
+		};
+	window.Spread =  function(a){
 		var r = [];
 		sp(r, arguments.length > 1 ? arguments : a);
 		return r;
