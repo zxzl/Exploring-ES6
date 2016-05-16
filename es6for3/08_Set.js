ecbe2bc7751c404d7286c62cfbcd6bbfc09d4e66
hika, maeng <hika00@gmail.com>
2016-05-17 01:05:50 +0900

    Update 08_Set.js

--- es6for3/08_Set.js
+++ es6for3/08_Set.js
@@ -1,11 +1,14 @@
-if(!window.Set) (function(){
+if(!window.Set || bsTest) (function(){
 	var SET = Symbol(), Set = function(a){
-		var set = this[SET] = [], self = this;
-		if(a instanceof Array) a.forEach(function(v){self.add(v);});
+		this[SET] = a instanceof Array ? a.slice(0) : [];
 		Object.freeze(this);
 	}, fn = Set.prototype;
 	fn.has = function(v){return this[SET].indexOf(v) != -1;};
 	fn.add = function(v){if(!this.has(v)) this[SET].push(v);};
-	fn.del = function(v){if(this.has(v)) this[SET].splice(this[SET].indexOf(v), 1);};
+	fn['delete'] = function(v){if(this.has(v)) this[SET].splice(this[SET].indexOf(v), 1);};
+	fn.clear = function(){this[SET].length = 0;};
+	fn.values = function(){return this[SET].slice(0);};
+	fn.forEach = function(f){for(var set = this[SET], i = 0, j = set.length; i < j; i++) f(set[i], i, this);};
+	fn[Symbol.iterator] = function(){return this[SET][Symbol.iterator]();};
 	window.Set = Set;
 })();
