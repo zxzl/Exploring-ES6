9921012c8a23e4e41bdfa6d913ed196d55326bd4
hika, maeng <hika00@gmail.com>
2016-05-16 16:34:08 +0900

    Update 10_generator.js

--- es6for3/10_generator.js
+++ es6for3/10_generator.js
@@ -1,20 +1,13 @@
 try{
 	function*(){};
 }catch(e){
-	var YVAL = Symbol();
-	(function(){
-		var y = function(v){
-			y[YVAL].push(v);
-		};
-		return y;
-	})();
 	window.Generator = function(f){
 		return function(){
 			var r = [];
 			window.Yield = function(v){r[r.length] = v;};
-			f();
+			f.apply(null, arguments);
 			delete window.Yield;
 			return new Iter(r);
 		};
 	};
-};
+}
