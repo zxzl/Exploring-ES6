4517263f792b12aba8622d2227b8888c9ac1797c
hika, maeng <hika00@gmail.com>
2016-05-17 00:37:08 +0900

    Update 07_WeakMap.js

--- es6for3/07_WeakMap.js
+++ es6for3/07_WeakMap.js
@@ -1,37 +1,16 @@
-if(!window.WeakMap) (function(){
-	var KEY = Symbol(), MAP = Symbol(), ID = Symbol(), uuid = 0, WMap = function(a){
-		var map = this[MAP] = {};
-		if(a instanceof Array) a.forEach(function(v){self.set(v[0], v[1]);});
-		Object.freeze(this);
-	}, fn = WMap.prototype;
-	fn.set = function(k, v){
-		if(k && typeof k == 'object' || typeof k == 'function'){
-			if(!k[ID]) k[ID] = '@WeakMap:' + (uuid++);
-			k = [k[ID]];
-		}
-		this[MAP][k] = v;
-	};
-	fn.get = function(k){
-		if(k && typeof k == 'object' || typeof k == 'function'){
-			if(k[ID]) k = k[ID];
-			else return;
-		}
-		return this[MAP][k];
-	};
-	fn.has = function(k){
-		if(k && typeof k == 'object' || typeof k == 'function'){
-			if(k[ID]) k = k[ID];
-			else return false;
-		}
-		return this[MAP].hasOwnProperty(k);
-	};
-	fn.del = function(k){
-		var v;
-		if(k && typeof k == 'object' || typeof k == 'function'){
-			if(k[ID]) v = k[ID], delete k[ID], k = v;
-			else return false;
-		}
-		delete this[MAP][k];
-	};
+if(!window.WeakMap || bsTest) (function(){
+	var KEY = Symbol(), MAP = Symbol(), ID = Symbol(),
+		type = {'object':1, 'function':1},
+		check = function(k){return k && k[ID] && type[typeof k];},
+		WMap = function(a){
+			var map = this[MAP] = {};
+			if(a instanceof Array) a.forEach(function(v){self.set(v[0], v[1]);});
+			Object.freeze(this);
+		},
+		fn = WMap.prototype;
+	fn.set = function(k, v){if(k && type[typeof k]) this[MAP][k[ID] || (k[ID] = Symbol() + '')] = v;};
+	fn.get = function(k){if(check(k)) return this[MAP][k[ID]];};
+	fn.has = function(k){return check(k) ? this[MAP].hasOwnProperty(k[ID]) :false;};
+	fn['delete'] = function(k){return check(k) ? delete this[MAP][k[ID]] && delete k[ID] : false;};
 	window.WeakMap = WMap;
 })();
