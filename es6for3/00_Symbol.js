f48ecc956486b5fedfca0e534b39fb4e7504c392
hika, maeng <hika00@gmail.com>
2016-05-16 11:11:13 +0900

    Update 00_Symbol.js

--- es6for3/00_Symbol.js
+++ es6for3/00_Symbol.js
@@ -1,20 +1,24 @@
 "use strict"
 if(typeof Object.freeze != 'function') Object.freeze = function(v){return v;};
-if(!window.Symbol1)(function(){
-	var uuid = 1,
-		Sym = function(id){this[ID] = id;},
-		Symbol1 = function(desc) {
-			return Object.freeze(new Sym('@@Symbol1@@::' + (desc || uuid++)));
+if(!window.Symbol)(function(){
+	var uuid = 0,
+		getId = function(){
+			return '@@Symbol:' + (+new Date) + ':' + (uuid++) + ':' + (Math.random() + '').substr(0, 5);
 		},
-		ID = '@@Symbol1@@::0',
+		ID = getId(),
+		Sym = function(id){
+			this[ID] = id;
+			return Object.freeze(this);
+		},
+		Symbol = function(){return new Sym(getId());},
 		keys = {},
 		syms = {};
 	Sym.prototype.toString = function(){return this[ID];},
-	Symbol1.for = function(k) {
-		if(!keys[k]) syms[keys[k] = Symbol1(k)] = k;
+	Symbol['for'] = function(k) {
+		if(!keys[k]) syms[keys[k] = Symbol(k)] = k;
 		return keys[k];
 	};
-	Symbol1.keyFor = function(s){return syms[s];};
-	Symbol1.iterator = Object.freeze(new Sym('@@iterator'));
-	window.Symbol1 = Object.freeze(Symbol1);
+	Symbol.keyFor = function(s){return syms[s];};
+	Symbol.iterator = Object.freeze(new Sym('@@iterator'));
+	window.Symbol = Object.freeze(Symbol);
 })();
