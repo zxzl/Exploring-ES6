fbbd8f5f73be3e2f398cd1357c04e913a826a472
hika, maeng <hika00@gmail.com>
2016-05-17 00:25:15 +0900

    Update 10_generator.js

--- es6for3/10_generator.js
+++ es6for3/10_generator.js
@@ -1,13 +1,9 @@
-try{
-	function*(){};
-}catch(e){
-	window.Generator = function(f){
-		return function(){
-			var r = [];
-			window.Yield = function(v){r[r.length] = v;};
-			f.apply(null, arguments);
-			delete window.Yield;
-			return new Iter(r);
-		};
+if(!window.Generator) window.Generator = function(f){
+	return function(){
+		var r = [];
+		window.Yield = function(v){r[r.length] = v;};
+		f.apply(null, arguments);
+		delete window.Yield;
+		return new Iter(r);
 	};
-}
+};
