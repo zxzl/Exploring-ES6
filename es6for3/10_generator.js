e946f6be0575cdbe506686a4643a7e70da3650b9
javarouka(이항희) <javarouka@coupang.com>
2016-05-17 10:32:18 +0900

    #1 ES6for 프로젝트 이전

--- es6for3/10_generator.js
+++
@@ -1,9 +0,0 @@
-if(!window.Generator) window.Generator = function(f){
-	return function(){
-		var r = [];
-		window.Yield = function(v){r[r.length] = v;};
-		f.apply(null, arguments);
-		delete window.Yield;
-		return new Iter(r);
-	};
-};
